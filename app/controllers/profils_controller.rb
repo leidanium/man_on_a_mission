f736db07b19ca05c035a056cd7087ae3fb025afc
leidanium <alexsam.quentin@gmail.com>
2022-03-22 09:50:55 +0100

    Setup Profils and add it as default route

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -1,10 +1,19 @@
 class ProfilsController < ApplicationController
+  before_action :set_profil, only: %i[ show ]
+
   def index
+    @profils = User.all
   end
 
   def search
   end
 
   def show
+  end
+
+  private 
+
+  def set_profil
+    @profil = User.find(params[:id])
   end
 end
