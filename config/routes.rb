6b43f3eb6ca3e2cbea5c8df9b01dba291e47e4a4
leidanium <alexsam.quentin@gmail.com>
2022-03-22 09:31:55 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'profils/index'
   get 'profils/search'
   get 'profils/show'
+
   resources :experiences
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   resources :skills
 
 
-  root "skills#index"
+  root "profils#index"
 
   # Defines the root path route ("/")
 end
