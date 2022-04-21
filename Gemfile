9214589e52a3bc554633d4e70798c64f178c0a8f
leidanium <alexsam.quentin@gmail.com>
2022-04-21 09:38:44 +0200

    Add Bootstrap

--- Gemfile
+++ Gemfile
@@ -48,6 +48,10 @@ gem 'devise'
 
 gem 'awesome_print'
 
+# Bootstrap
+gem "sassc-rails"
+gem 'bootstrap', '~> 5.1.3'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
@@ -10,3 +12,5 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
